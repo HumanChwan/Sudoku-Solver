15c9e1785cc7eddaddcc0e6c4b276ad16b3ac07a
DankusMaximus <41588684+DankusMaximus@users.noreply.github.com>
2020-12-31 02:37:00 +0530

    added a display func

--- SudSolv.cpp
+++ SudSolv.cpp
@@ -3,10 +3,22 @@
 #define pb push_back
 
 using namespace std;
-
+const int r = 9, c = 9;
+vector<vector<int>> S(r, vector<int>(c));
+void Display(){
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            if(j != c-1) cout<<S[i][j]<<" ";
+            else cout<<S[i][j];
+            if(j == 2 || j == 5) cout<<" ";
+        }
+        if(i != r-1) cout<<"\n";
+        else cout<<"\n\n------------------------";
+        if(i == 2 || i == 5) cout<<"\n";
+    }
+}
 int main(){
-    const int r = 9, c = 9;
-    vector<vector<int>> S(r, vector<int>(c));
-    //for(auto& Si : S) for(auto& Sij : Si) cin>>Sij; //input(we could potentially hard code the input ig)
-    
+    for(auto& Si : S) for(auto& Sij : Si) cin>>Sij;
+    Display();
+    return 0;
 }
