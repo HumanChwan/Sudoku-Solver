4c511cabe6fbb459a9401d79e14e267c0124b3b3
DankusMaximus <41588684+DankusMaximus@users.noreply.github.com>
2020-12-30 16:35:17 +0530

    input style alpha

--- SudSolv.cpp
+++ SudSolv.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include <vector>
 #define pb push_back
-#define ll long long
-#define M 1000000007
-#define fl(i, a, b) for(int i = a; i < b; i++)
 
 using namespace std;
 
 int main(){
     const int r = 9, c = 9;
     vector<vector<int>> S(r, vector<int>(c));
+    //for(auto& Si : S) for(auto& Sij : Si) cin>>Sij; //input(we could potentially hard code the input ig)
     
 }
