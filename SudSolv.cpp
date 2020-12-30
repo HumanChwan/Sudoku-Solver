5404ba772887848bbee7b481c7afb5eda7b37d9d
DankusMaximus <41588684+DankusMaximus@users.noreply.github.com>
2020-12-31 02:45:04 +0530

    small update

--- SudSolv.cpp
+++ SudSolv.cpp
@@ -2,23 +2,22 @@
 #include <vector>
 #define pb push_back
 
-using namespace std;
 const int r = 9, c = 9;
-vector<vector<int>> S(r, vector<int>(c));
+std :: vector<std :: vector<int>> S(r, std :: vector<int>(c));
 void Display(){
     for(int i = 0; i < r; i++){
         for(int j = 0; j < c; j++){
-            if(j != c-1) cout<<S[i][j]<<" ";
-            else cout<<S[i][j];
-            if(j == 2 || j == 5) cout<<" ";
+            if(j != c-1) std :: cout << S[i][j] << " ";
+            else std :: cout << S[i][j];
+            if(j == 2 || j == 5) std :: cout << " ";
         }
-        if(i != r-1) cout<<"\n";
-        else cout<<"\n\n------------------------";
-        if(i == 2 || i == 5) cout<<"\n";
+        if(i != r-1) std :: cout << "\n";
+        else std :: cout << "\n------------------------";
+        if(i == 2 || i == 5) std :: cout << "\n";
     }
 }
 int main(){
-    for(auto& Si : S) for(auto& Sij : Si) cin>>Sij;
+    for(auto& Si : S) for(auto& Sij : Si) std :: cin >> Sij;
     Display();
     return 0;
 }
