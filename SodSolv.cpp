b9b496ff7db7ce1df358db1f981ae3569d791860
HumanChwan <dineshchukkala169@gmail.com>
2021-01-21 11:00:11 +0530

    erase/remove idiom added

--- SodSolv.cpp
+++ SodSolv.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <set>
 #include <algorithm>
 #define pb push_back
 #define fl(i, a, b) for(int i = a; i < b; ++i)
@@ -9,7 +10,7 @@ using namespace std;
 const int r = 9, c = 9;
 int S[r][c];
 vector < int > b_i;
-int row[9], column[9], square[9];
+set < int > row, column, square;
 void Input(){
     fl(i, 0, r) fl(j, 0, c) cin >> S[i][j];
     return;
@@ -34,40 +35,26 @@ void blank_init(){
             if(!S[i][j]) b_i.pb(i*10 + j);
         }
     }
-    //fl(i, 0, b_i.size()) cout << b_i[i] << " ";
 }
 void mod_init(int rm){
-    int start = 0;
-    int ender = b_i.size();
-    vector < int >::iterator it;
-    it = b_i.begin();
-    while(start <= ender){
-        int mid = (start + ender)/2;
-        if(b_i[mid] == rm){
-            advance(it, mid);
-            b_i.erase(it);
-            break;
-        }
-        else if(b_i[mid] > rm){
-            ender = mid;
-        }
-        else{
-            start = mid + 1;
-        }
-    }
+    b_i.erase( remove(b_i.begin(), b_i.end(), rm), b_i.end());
+    return;
 }
 void row_init(int rn){
-    fl(i, 0, r) row[i] = S[rn][i];
+    row.clear();
+    fl(i, 0, r) row.insert(S[rn][i]);
     return;
 }
 void column_init(int cn){
-    fl(i, 0, c) column[i] = S[i][cn];
+    column.clear();
+    fl(i, 0, r) row.insert(S[i][cn]);
     return;
 }
 void square_init(int sn){
+    square.clear();
     int row_index = ((sn/10)/3) * 3;
     int column_index = ((sn%10)/3) * 3;
-    fl(i, 0, r/3) fl(j, 0, c/3) square[(i*3) + j] = S[row_index + i][column_index + j];
+    fl(i, 0, r/3) fl(j, 0, c/3) square.insert(S[row_index + i][column_index + j]);
     return;
 }
 void Solve(){
@@ -83,20 +70,16 @@ void brute_solve(){
             row_init(b_i[i]/10);
             column_init(b_i[i]%10);
             square_init(b_i[i]);
-            sort(row, row+r);
-            sort(column, column+c);
-            sort(square, square+(r/3 + c/3));
             vector < int > row_empty{1, 2, 3, 4, 5, 6, 7, 8, 9}, column_empty{1, 2, 3, 4, 5, 6, 7, 8, 9},
                     square_empty{1, 2, 3, 4, 5, 6, 7, 8, 9};
-            //vector < int >::iterator it;
             for(auto it = row_empty.begin(); it != row_empty.end(); it++){
-                if(binary_search(row, row+r, *it)) row_empty.erase(it), --it;
+                if(binary_search(row.begin(), row.end(), *it)) row_empty.erase(it), --it;
             }
             for(auto it = column_empty.begin(); it != column_empty.end(); it++){
-                if(binary_search(column, column+c, *it)) column_empty.erase(it), --it;
+                if(binary_search(column.begin(), column.end(), *it)) column_empty.erase(it), --it;
             }
             for(auto it = square_empty.begin(); it != square_empty.end(); it++){
-                if(binary_search(square, square+(r/3 + c/3), *it)) square_empty.erase(it), --it;
+                if(binary_search(square.begin(), square.end(), *it)) square_empty.erase(it), --it;
             }
             vector < int > temp, final;
             for(auto it = row_empty.begin(); it != row_empty.end(); ++it){
@@ -105,7 +88,6 @@ void brute_solve(){
             for(auto it = temp.begin(); it != temp.end(); ++it){
                 if(binary_search(square_empty.begin(), square_empty.end(), *it)) final.pb(*it);
             }
-
             if(final.size() == 1){
                 S[b_i[i]/10][b_i[i]%10] = final[0];
                 mod_init(b_i[i]);
