eb4547e98e72349ad7ee1e75783ac50b01d5b832
HumanChwan <dineshchukkala169@gmail.com>
2021-02-02 23:40:15 +0530

    Done till hard level

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 Language : C++  
 EasyGrid.txt contains Sudoku grids with grids which are categorized as `easy` to solve  
 MedGrid.txt contains Sudoku grids with grids which are categorized as `medium` to solve   
-HardGrid.txt contains Sudoku grids with grids which are categorized as `medium` to solve      
+HardGrid.txt contains Sudoku grids with grids which are categorized as `hard` to solve
+ExpertGrid.txt contains Sudoku grids with grids which are categorized as `expert` to solve      
 
 `easy` : requires brute force  
 `medium` : requires brute force along with Numerical Solving  
-`hard` : requires methods according to requirement     
+`hard` : requires elimination of possible cases 
+`expert` : requires to take an assumption based on a simple probability, and thereby applting other ways i.e. brute force and such     
 
-`SodSolv.cpp` : C++14/C++17/C++20 file  
-
-test test test
+`SodSolv.cpp` : C++14/C++17 file  
--- SodSolv.cpp
+++ SodSolv.cpp
@@ -37,7 +37,7 @@ void blank_init(){
     }
 }
 void mod_init(int rm){
-    b_i.erase( remove(b_i.begin(), b_i.end(), rm), b_i.end());
+    b_i.erase(remove(b_i.begin(), b_i.end(), rm), b_i.end());
     return;
 }
 void row_init(int rn){
@@ -47,7 +47,7 @@ void row_init(int rn){
 }
 void column_init(int cn){
     column.clear();
-    fl(i, 0, r) row.insert(S[i][cn]);
+    fl(i, 0, r) column.insert(S[i][cn]);
     return;
 }
 void square_init(int sn){
@@ -59,8 +59,20 @@ void square_init(int sn){
 }
 void Solve(){
     void brute_solve();
+    void num_solve();
+    void eliminator_solve();
     blank_init();
-    brute_solve();
+    int temp = b_i.size();
+    while(true){
+        temp = b_i.size();
+        brute_solve();
+        if(!b_i.size()) break;
+        num_solve();
+        if(!b_i.size()) break;
+        if(temp == b_i.size())
+            eliminator_solve();
+        if(temp == b_i.size() || !b_i.size()) break;
+    }
 }
 void brute_solve(){
 
@@ -70,26 +82,18 @@ void brute_solve(){
             row_init(b_i[i]/10);
             column_init(b_i[i]%10);
             square_init(b_i[i]);
-            vector < int > row_empty{1, 2, 3, 4, 5, 6, 7, 8, 9}, column_empty{1, 2, 3, 4, 5, 6, 7, 8, 9},
-                    square_empty{1, 2, 3, 4, 5, 6, 7, 8, 9};
-            for(auto it = row_empty.begin(); it != row_empty.end(); it++){
-                if(binary_search(row.begin(), row.end(), *it)) row_empty.erase(it), --it;
+            vector < int > netcnt{1, 2, 3, 4, 5, 6, 7, 8, 9};
+            for(auto it = netcnt.begin(); it != netcnt.end(); it++){
+                if(binary_search(row.begin(), row.end(), *it)) netcnt.erase(it), --it;
             }
-            for(auto it = column_empty.begin(); it != column_empty.end(); it++){
-                if(binary_search(column.begin(), column.end(), *it)) column_empty.erase(it), --it;
+            for(auto it = netcnt.begin(); it != netcnt.end(); it++){
+                if(binary_search(column.begin(), column.end(), *it)) netcnt.erase(it), --it;
             }
-            for(auto it = square_empty.begin(); it != square_empty.end(); it++){
-                if(binary_search(square.begin(), square.end(), *it)) square_empty.erase(it), --it;
+            for(auto it = netcnt.begin(); it != netcnt.end(); it++){
+                if(binary_search(square.begin(), square.end(), *it)) netcnt.erase(it), --it;
             }
-            vector < int > temp, final;
-            for(auto it = row_empty.begin(); it != row_empty.end(); ++it){
-                if(binary_search(column_empty.begin(), column_empty.end(), *it)) temp.pb(*it);
-            }
-            for(auto it = temp.begin(); it != temp.end(); ++it){
-                if(binary_search(square_empty.begin(), square_empty.end(), *it)) final.pb(*it);
-            }
-            if(final.size() == 1){
-                S[b_i[i]/10][b_i[i]%10] = final[0];
+            if(netcnt.size() == 1){
+                S[b_i[i]/10][b_i[i]%10] = netcnt[0];
                 mod_init(b_i[i]);
                 if(!b_i.size()) return;
             }
@@ -97,12 +101,123 @@ void brute_solve(){
         if(temp == b_i.size()) return;
     }
 }
+void num_solve(){
+    while(true){
+        int temp = b_i.size();
+        fl(i, 1, 10){
+            fl(j, 0 , 9){
+                square_init((j/3)*30 + (j%3)*3);
+                if(!binary_search(square.begin(), square.end(), i)){
+                    bool ra[3] = {false, false, false}, ca[3] = {false, false, false};
+
+                    row_init((j/3)*3);
+                    if(!binary_search(row.begin(), row.end(), i)) ra[0] = true;
+                    row_init((j/3)*3 + 1);
+                    if(!binary_search(row.begin(), row.end(), i)) ra[1] = true;
+                    row_init((j/3)*3 + 2);
+                    if(!binary_search(row.begin(), row.end(), i)) ra[2] = true;
+
+                    column_init((j%3)*3);
+                    if(!binary_search(column.begin(), column.end(), i)) ca[0] = true;
+                    column_init((j%3)*3 + 1);
+                    if(!binary_search(column.begin(), column.end(), i)) ca[1] = true;
+                    column_init((j%3)*3 + 2);
+                    if(!binary_search(column.begin(), column.end(), i)) ca[2] = true;
+
+                    int ct = 0, fill;
+                    fl(k, 0, 3){
+                        if(!ra[k]) continue;
+                        fl(l, 0, 3){
+                            if(ca[l] && !S[(j/3)*3 + k][(j%3)*3 + l]){
+                                ct++;
+                                if(ct > 1) break;
+                                fill = ((j/3)*3 + k)*10 + ((j%3)*3 + l);
+                            }
+                        }
+                        if(ct > 1) break;
+                    }
+                    if(ct == 1){
+                        S[fill/10][fill%10] = i;
+                        mod_init(fill);
+                        if(!b_i.size()) return;
+                    }
+                }
+            }
+        }
+        if(temp == b_i.size()) return;
+    }
+}
+void eliminator_solve(){
+    while(true){
+        int temp = b_i.size();
+        fl(i, 0, 9){
+            vector < int > net{1, 2, 3, 4, 5, 6, 7, 8, 9};
+            row_init(i);
+            for(auto it = net.begin(); it != net.end(); it++){
+                if(binary_search(row.begin(), row.end(), *it)) net.erase(it), --it;
+            }
+            for(auto it = net.begin(); it != net.end(); it++){
+                int ct = 0, fill;
+                fl(j, 0, 9){
+                    if(!S[i][j]){
+                        column_init(j);
+                        if(binary_search(column.begin(), column.end(), *it)) continue;
+                        square_init(i*10 + j);
+                        if(binary_search(square.begin(), square.end(), *it)) continue;
+                        ct++;
+                        fill = i*10 + j;
+                    }
+                }
+                if(ct == 1){
+                    S[fill/10][fill%10] = *it;
+                    mod_init(fill);
+                    if(!b_i.size()) return;
+                }
+            }
+        }
+        fl(j, 0, 9){
+            vector < int > net{1, 2, 3, 4, 5, 6, 7, 8, 9};
+            column_init(j);
+            for(auto it = net.begin(); it != net.end(); it++){
+                if(binary_search(column.begin(), column.end(), *it)) net.erase(it), --it;
+            }
+            for(auto it = net.begin(); it != net.end(); it++){
+                int ct = 0, fill;
+                fl(i, 0, 9){
+                    if(!S[i][j]){
+                        row_init(i);
+                        if(binary_search(row.begin(), row.end(), *it)) continue;
+                        square_init(i*10 + j);
+                        if(binary_search(square.begin(), square.end(), *it)) continue;
+                        ct++;
+                        fill = i*10 + j;
+                    }
+                }
+                if(ct == 1){
+                    S[fill/10][fill%10] = *it;
+                    mod_init(fill);
+                    if(!b_i.size()) return;
+                }
+            }
+        }
+        if(temp == b_i.size()) return;
+    }
+}
+
+void probable_solve(){
+    
+}
+
+
 int main(){
     Input();
-    //for(int i = 0; i < r; ++i) for(int j = 0; j < c; ++j) std :: cin >> S[i][j];
     Display();
     blank_init();
     Solve();
+    if(b_i.size())
+        probable_solve();
+    if(b_i.size())
+        cout << "Has either more than one solution or no solution\n"; 
     Display();
     return 0;
 }
